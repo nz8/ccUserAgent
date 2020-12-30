14c569e32489e6aba356ceca4822f09db91f2edc
nz8 <60668100+nz8@users.noreply.github.com>
2020-12-29 21:45:46 -0600

    Update ccUserAgent.js

--- ccUserAgent.js
+++ ccUserAgent.js
@@ -1,5 +1,5 @@
 /*  ccUserAgent(current Chrome User Agent) is a Node.js module that determines the user agent string for
-*   the latest stable Chrome release release available for Windows 10 and running
+*   the latest stable Chrome release available for Windows 10 and running
 *   on Windows 10. It does so by first locating the latest partial version string
 *   (containing the first 3 components but missing the fourth) within Wikipedia's
 *   Chrome version history table, then searching through Google Chrome release blog
